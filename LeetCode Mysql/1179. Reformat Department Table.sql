587ad4143c60d2f740cbb9720ddde0e470ed40b7
Collin <sghzczy@gmail.com>
2021-01-07 17:28:53 +0800

    1/7

--- LeetCode Mysql/1179. Reformat Department Table.sql
+++ LeetCode Mysql/1179. Reformat Department Table.sql
@@ -1,17 +1,17 @@
 # Write your MySQL query statement below
 select id,
-    sum(case when month='jan' then revenue end) as jan_revenue,
-    sum(case when month='feb' then revenue end) as feb_revenue,
-    sum(case when month='mar' then revenue end) as mar_revenue,
-    sum(case when month='apr' then revenue end) as apr_revenue,
-    sum(case when month='may' then revenue end) as may_revenue,
-    sum(case when month='jun' then revenue end) as jun_revenue,
-    sum(case when month='jul' then revenue end) as jul_revenue,
-    sum(case when month='aug' then revenue end) as aug_revenue,
-    sum(case when month='sep' then revenue end) as sep_revenue,
-    sum(case when month='oct' then revenue end) as oct_revenue,
-    sum(case when month='nov' then revenue end) as nov_revenue,
-    sum(case when month='dec' then revenue end) as dec_revenue
-from department
+    sum(case when month='jan' then revenue end) as Jan_revenue,
+    sum(case when month='feb' then revenue end) as Feb_revenue,
+    sum(case when month='mar' then revenue end) as Mar_revenue,
+    sum(case when month='apr' then revenue end) as Apr_revenue,
+    sum(case when month='may' then revenue end) as May_revenue,
+    sum(case when month='jun' then revenue end) as Jun_revenue,
+    sum(case when month='jul' then revenue end) as Jul_revenue,
+    sum(case when month='aug' then revenue end) as Aug_revenue,
+    sum(case when month='sep' then revenue end) as Sep_revenue,
+    sum(case when month='oct' then revenue end) as Oct_revenue,
+    sum(case when month='nov' then revenue end) as Nov_revenue,
+    sum(case when month='dec' then revenue end) as Dec_revenue
+from Department
 group by id
 order by id;
