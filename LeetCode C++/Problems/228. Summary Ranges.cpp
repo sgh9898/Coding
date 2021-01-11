7fa18ccb784f58a049c4b03ce60ee3a795d7f6a8
Collin <sghzczy@gmail.com>
2021-01-11 17:05:26 +0800

    1/11

---
+++ LeetCode C++/Problems/228. Summary Ranges.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+        int left = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i + 1 == nums.size() || nums[i] + 1 != nums[i + 1]) {
+                ans.push_back(to_string(nums[left]) + (left == i ? "" : "->" + to_string(nums[i])));
+                left = i + 1;
+            }
+        }
+        return ans;
+    }
+};
