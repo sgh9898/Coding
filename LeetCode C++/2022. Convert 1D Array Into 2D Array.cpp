3978b15e416d10370fc5a576dfa941310f2d9b52
Collin <sgh0429@163.com>
2022-01-05 15:51:55 +0800

    1/5

---
+++ LeetCode C++/2022. Convert 1D Array Into 2D Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int> &original, int m, int n) {
+        vector<vector<int>> ans;
+
+        // impossible
+        if (original.size() != m * n) {
+            return ans;
+        }
+        
+        for (auto it = original.begin(); it != original.end(); it += n) {
+            ans.emplace_back(it, it + n);
+        }
+        return ans;
+    }
+};
